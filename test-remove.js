404c149aadc02f8641285fd984b4447411db1266
rodolfomatos <rodolfo.matos@gmail.com>
2026-05-06 23:25:25 +0100

    chore: remove temporary test/debug files

--- test-remove.js
+++
@@ -1,5 +0,0 @@
-import * as cheerio from 'cheerio';
-const html = `<html><head><script>var a = 1;</script></head><body><p>Hello</p></body></html>`;
-const $ = cheerio.load(html);
-$('script').remove();
-console.log('After remove:', $.text());
